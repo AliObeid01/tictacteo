7b687dd23f7cd529c6538788f8f6021b2d0cce0d
unknown <aliiobeid01@gmail.com>
2022-09-16 18:45:24 +0300

    create the button eventlistener and a function game to start the game

--- index.js
+++ index.js
@@ -15,3 +15,33 @@ let conditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
+
+reset.addEventListener('click', restart);
+//loop on each button click
+btns.forEach((btn, i) => {
+    btn.addEventListener('click', () => game(btn, i));
+});
+
+const game = (element, index) => {
+    element.value = player;
+    element.disabled = true;
+    cells[index] = player;
+    player = player == 'X' ? 'O' : 'X';
+    turn.innerHTML = `Player ${player} Turn`;
+//loop to check all the condition posibilty as declared above
+    for (let i = 0; i < conditions.length; i++) {
+        let condition = conditions[i];
+        let cond0 = cells[condition[0]];
+        let cond1 = cells[condition[1]];
+        let cond2 = cells[condition[2]];
+
+        if (cond0 == '' || cond1 == '' || cond2 == '') {
+            continue;
+        }
+         //if a condition match we get the player winner
+        if ((cond0 == cond1) && (cond1 == cond2)) {
+            turn.innerHTML = `Player ${cond0} Win`;
+            btns.forEach((btn) => btn.disabled = true);
+        }
+    }
+};
